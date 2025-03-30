1d88530f03d1252ddbbec532f25a67570628e106
kj15239 <kj15239@naver.com>
2025-03-31 01:20:43 +0900

    주석처리 + redis 로그아웃

--- src/main/java/test/crudboard/entity/Post.java
+++ src/main/java/test/crudboard/entity/Post.java
@@ -32,7 +32,7 @@ public class Post {
     private String context;
     private Long view;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
